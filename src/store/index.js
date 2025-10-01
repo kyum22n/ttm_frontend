5675c30726e537c9f551bd993596771836dfbcb2
bale0610 <see23069@naver.com>
2025-10-01 21:34:11 +0900

    25.10.01 kbh 펫 이미지 불러오기

--- src/store/index.js
+++ src/store/index.js
@@ -14,6 +14,7 @@ const store = createStore({
       userEmail: "",
       userAddress: "",
       userBirthDate: "",
+      profileImage: "",
     },
     signupUser: null, // 회원가입 단계에서 임시 저장
     jwt: "",
