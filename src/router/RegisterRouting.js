6c04b7284d72ec9dfad26bca22ac2768878e1ac8
bale0610 <see23069@naver.com>
2025-10-01 11:26:13 +0900

    25.10.01 kbh 회원가입 구현완(공부해야함)

--- src/router/RegisterRouting.js
+++ src/router/RegisterRouting.js
@@ -1,11 +1,13 @@
 const routes = [
   {
     path: "/Register/Pet",
+    name: "PetRegister",
     component: () => import(/*webpackChunkName: "RegisterRouting"*/ "@/views/Register/RegisterPet"),
     meta:{hideLayout:true}
   },
   {
     path: "/Register/User",
+    name: "RegisterUser", 
     component: () => import(/*webpackChunkName: "RegisterRouting"*/ "@/views/Register/RegisterUser"),
     meta:{hideLayout:true}
   },
